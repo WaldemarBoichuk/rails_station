fe9d2e84c4f20969b17653405e1c2a47d090f0e1
volodymyrboichuk <waldemar.bojczuk@gmail.com>
2017-12-02 10:08:42 +0100

    Add basic CRUD for train

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :railways
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'railways/index'
+
+  root 'railways#index'
 end
